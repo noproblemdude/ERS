7cf14eed4a996565a753c95ed324f68d9a47e685
noproblemdude <leonardrolandvictor@googlemail.com>
2023-07-08 17:10:28 +0200

    Neues CAN library

--- src/BSW/CAN_driver.cpp
+++ src/BSW/CAN_driver.cpp
@@ -1,37 +1,59 @@
 #include "CAN_driver.h"
 
 
-mcp2515_can CAN(SPI_CS_PIN);
+
+long unsigned int rxId;
+unsigned char len = 0;
+unsigned char rxBuf[8];
+char msgString[128];
+//mcp2515_can CAN(SPI_CS_PIN);
 unsigned char flagRecv;
-unsigned char len;
-unsigned char buf[8];
+//unsigned char len;
+//unsigned char buf[8];
 char str[20];
 extern unsigned char stmp[8];
 
+MCP_CAN CAN0(49);
 
+/*
 void MCP2515_ISR(void) {
       flagRecv = 1;
 }
-
+*/
 void CAN_setup(void){
  
+    Serial.begin(115200);
 
-    SERIAL_PORT_MONITOR.begin(115200);
-    while (!SERIAL_PORT_MONITOR) {
-        ; // wait for serial port to connect. Needed for native USB port only
-    }
-    attachInterrupt(digitalPinToInterrupt(CAN_INT_PIN), MCP2515_ISR, FALLING); // start interrupt
-    while (CAN_OK != CAN.begin(CAN_500KBPS)) {             // init can bus : baudrate = 500k
-        SERIAL_PORT_MONITOR.println("CAN init fail, retry...");
-        delay(100);
-    }
-    SERIAL_PORT_MONITOR.println("CAN init ok!");
+
+    // Initialize MCP2515 running at 16MHz with a baudrate of 500kb/s and the masks and filters disabled.
+    if(CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_16MHZ) == CAN_OK)
+        Serial.println("MCP2515 Initialized Successfully!");
+    else
+        Serial.println("Error Initializing MCP2515...");
+    
+    CAN0.setMode(MCP_NORMAL);                     // Set operation mode to normal so the MCP2515 sends acks to received data.
+
+    pinMode(CAN0_INT, INPUT);                            // Configuring pin for /INT input
 
 
 }
 
+byte data[8] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07};
 
 
+void sendTest()
+{
+  // send data:  ID = 0x100, Standard CAN Frame, Data length = 8 bytes, 'data' = array of data bytes to send
+  byte sndStat = CAN0.sendMsgBuf(0x100, 0, 8, data);
+  if(sndStat == CAN_OK){
+    Serial.println("Message Sent Successfully!");
+  } else {
+    Serial.println("Error Sending Message...");
+  }
+  delay(100);   // send data per 100ms
+}
+
+/*
 void CAN_listen(unsigned char *buffer){
 
     unsigned int time = 0;
@@ -61,3 +83,4 @@ void CAN_transmit(const byte *body){
 
 
 
+*/
